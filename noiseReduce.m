9e538aaffa74b6d8cff4ed097966b5c296f222ec
Chris Dombele <christopher.dombele@gmail.com>
2019-08-05 15:36:05 -0400

    adding changes to noise

--- noiseReduce.m
+++ noiseReduce.m
@@ -1,40 +1,40 @@
 % %Removing noise with adaptive filtering 
-coinsImg = imread('coins.jpg');
-bw = rgb2gray(coinsImg);
-J = imnoise(bw,'gaussian',0,0.025);
+% coinsImg = imread('coins.jpg');
+% bw = rgb2gray(coinsImg);
+% J = imnoise(bw,'gaussian',0,0.025);
 % figure(1);
 % imshow(bw);
 % title('Black and White version');
 
-K = wiener2(bw,[5 5]);
+% K = wiener2(bw,[5 5]);
 % figure(2);
 % imshow(K);
 % title('Noise Removal');
 % figure(3);
 % imshow(coinsImg);
 % title('Original Image');
-L = wiener2(K,[5 5]);
+% L = wiener2(K,[5 5]);
 % figure(4);
 % imshow(L);
 
 % pause();
 % clc; clear all; close all;
 
-% %Remove Noise Using an Averaging Filter and a Median Filter
-% I = imread('mona_noisy.png');
-% bw = rgb2gray(I);
-% figure(1);
-% imshow(bw);
-% Kaverage = filter2(fspecial('average',3),bw)/255;
-% figure(2);
-% imshow(Kaverage);
-% 
+%Remove Noise Using an Averaging Filter and a Median Filter
+I = imread('coins.jpg');
+bw = rgb2gray(I);
+figure(1);
+imshow(bw);
+Kaverage = filter2(fspecial('average',3),bw)/255;
+figure(2);
+imshow(Kaverage);
+
 % pause();
 % clc; clear all; close all;
 
 
 % Contrast enhancing
-I2 = histeq(L);
+I2 = histeq(Kaverage);
 figure(5);
 imshow(I2);
 title('Histogram Equalization');
