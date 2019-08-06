157758ce5d456d6d67312744993c2789d9d633c5
Eli-Labell <paragonuber@gmail.com>
2019-08-05 20:21:50 -0400

    Add files via upload

--- idcoins.m
+++ idcoins.m
@@ -4,10 +4,14 @@ close all
 clear all %#ok<*CLALL>
 
 %function counts = idcoins(name)
-name = 'all.jpg'; %remove later
+name = 'c1.jpg'; %remove later
 
-ratios = [0.9083, 0.6500, 0.7156, 0.8953, 0.7259, 0.7993, 1/0.9083, 1/0.6500, 1/0.7156, 1/0.8953, 1/0.7259, 1/0.7993];
-coins = ["Penny", "Quarter", "Quarter", "Quarter", "Nickle", "Nickle", "dime", "dime", "Penny", "Nickle", "Dime", "Penny"];
+smol = 0.2;
+
+%ratios = [0.9083, 0.6500, 0.7156, 0.8953, 0.7259, 0.7993, 1/0.9083, 1/0.6500, 1/0.7156, 1/0.8953, 1/0.7259, 1/0.7993, 1];
+%coins = ["Penny", "Quarter", "Quarter", "Quarter", "Nickle", "Nickle", "dime", "dime", "Penny", "Nickle", "Dime", "Penny", "SELF"];
+ratios = [1.3004 1.1367 1.3695 0.769 0.8741 1.0531 0.8797 1.144 1.2048 0.7302 0.9496 0.83 1];
+coins = ["Penny" "Nickel" "Dime" "Quarter" "Nickel" "Dime" "Quarter" "Penny" "Dime" "Quarter" "Penny" "Nickel" "ME"];
 %denominators
 
 dp = 0.9083;
@@ -19,36 +23,40 @@ pn = 0.7993;
 
 A = imread(name);
 A = rgb2gray(A);
-A = histeq(A);
-% A = imbinarize(A);
-% A = abs(A - 1);
-% [circles, sizes] = zeros(1, 1), zeros(1,1);
-% size([circles])
-% 
-% imshow(A);
-% pause;
-% A = imfill(A, 'holes');
-% A = abs(A - 1);
-% imshow(A);
-% pause;
-% 
-% A = bwlabel(A, 4);
+A = filter2(fspecial('average',3), A) / 255;
+A = imresize(A, smol);
+B = histeq(A);
+B = B + 30;
+
+%
+I = double(A);
+a = min(min(I));
+b = max(max(I));
+t = 150;
+[m1,m2] = size(I);
+I2 = zeros(m1,m2);
+for i = 1:m1
+    for j = 1:m2
+        I2(i,j) = (t/(b-a))*(I(i,j)-a);
+    end
+end
+A = uint8(I2);
+
+
 % 
 imshow(A);
 % 
-% coloredLabels = label2rgb (A, 'hsv', 'k', 'shuffle'); % pseudo random color labels
-% imshow(coloredLabels);
 
-%pix = cast((max(size((A)))), 'uint8') %#ok<NOPTS>
+%%%testing
+%mask = zeros(size(A));
+%mask(1:end,1:end) = 1;
+%X = activecontour(A,mask, 8000, 'edge');
+%%%
+%imshow(X);
 
-%for pix = pix:-10:10
-%    [centers, radii] = imfindcircles(A, [pix-5, pix + 5]);
-%    if size(centers > 0)
-%        disp([centers, radii])    
-%    end
-%end
 
-[c, r] = imfindcircles(A, [200 1300], 'ObjectPolarity', 'dark', 'Sensitivity', 0.98) %#ok<NOPTS>
+[c, r] = imfindcircles(A, [smol*200 smol*1300], 'ObjectPolarity', 'dark', 'Sensitivity', 0.97) %#ok<NOPTS> %0.98
+%[c, r] = imfindcircles(A, [200 1300], 'ObjectPolarity', 'dark', 'Sensitivity', 0.99) %#ok<NOPTS> %0.98
 r = r*1.1;
 viscircles(c, r)
 
@@ -58,20 +66,7 @@ if max(size(c)) == 1
    n = match('nickel.pgm','test.pgm');
    d = match('dime.pgm', 'test.pgm');
    q = match('quarter.pgm', 'teast.pgm');
-   b = p;
-   v = 1;
-   if b < n
-       b = n;
-       v = 5;
-   end
-   if b < d
-       b = d;
-       v = 10;
-   end
-   if b < q
-       b = q;
-       v = 25;
-   end
+   
    
    %return v
 end
@@ -80,21 +75,40 @@ end
 
 if max(size(r)) > 1
     f = -1;
-    size(c);
-    nearness = zeros(size(c));
-    nearness = nearness + 10;
-    for i=1:size(c)
-        for j=1:size(c)
-            nearness(i, j) = (r(i) / r(j)) + 20;
-            for k=1:size(ratios)
-                if abs(ratios(k) - (r(i) / r(j))) < nearness(i, j)
-                    nearness(i, j) = abs(ratios(k) - (r(i) / r(j)))
-                    f = k
+    p = max(size(c))
+    nearness = zeros(p, p, 2);
+    nearness = nearness + 19;
+    for i=1:p %cols
+        for j=1:p %rows
+            nearness(i, j, 1) = (r(i) / r(j));
+            nearness(i, j, 2) = 1;
+            for k=1:max(size(ratios))
+                %disp(abs(ratios(k) - nearness(i, j, 1)))
+                %disp(abs(ratios(nearness(i, j, 2)) - nearness(i, j, 1)))
+                %disp(abs(ratios(k) - nearness(i, j, 1)) < abs(ratios(nearness(i, j, 2)) - nearness(i, j, 1)))
+                if abs(ratios(k) - nearness(i, j, 1)) < abs(ratios(nearness(i, j, 2)) - nearness(i, j, 1)) 
+                    nearness(i, j, 2) = k;
+                    
+                    %f = k
                 end
             end
-            nearness(i, j) = f;
+            
+            %nearness(i, j, 2) = coins(nearness(i, j, 2));
         end
     end
     nearness
 end
+
+
+% mask = zeros(size(A));
+% mask(1:end,1:end) = 1;
+% X = activecontour(A,mask, 8000, 'edge');
+% %Y = imopen(Y, strel('disk',5));
+% X = imclose(X, strel('disk',10));
+% %Y = imdilate(X, strel('disk',20));
+% Y = imfill(X, 'holes');
+% 
+% f1 = figure();
+% subplot(1, 2, 1), imshow(X);
+% subplot(1, 2, 2), imshow(Y);
 
